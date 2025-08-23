458c4ea9a82cea1d177eff881bf110701df5c704
PRSD <prsdsrinivas7ai@gmail.com>
2025-08-23 12:07:33 +0530

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from bot import flask_app
 
+# Make the Flask app available as 'app' for gunicorn
+app = flask_app
+
 if __name__ == '__main__':
-    flask_app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
